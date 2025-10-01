d9f0eadf8502303225fd50d1e4f13e2b0f3d66d5
MargaritaMolokova <shuashl@mail.ru>
2025-10-01 11:20:42 +0000

    dd

--- todo/src/components/Todo/Todo.jsx
+++ todo/src/components/Todo/Todo.jsx
@@ -1,9 +1,9 @@
-const Todo = () =>{
+function Todo ({name, toggleTodo, deleteTodo, id, done}) {
     return(
             <div className="todo-item">
-                <input type="checkbox" className="todo-checkbox"/>
-                <span className="todo-text">Изучить основы React</span>
-                <button className="delete-btn">Удалить</button>
+                <input onChange={() => toggleTodo(id)} checked={done} type="checkbox" className="todo-checkbox"/>
+                <span className="todo-text">{name}</span>
+                <button onClick={() => deleteTodo(id)} className="delete-btn">Удалить</button>
             </div>
     )
 }
